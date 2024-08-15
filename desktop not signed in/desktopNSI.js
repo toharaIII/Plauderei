00a506124db7cd586656ab20dab8bff11619fbfc
Thomas O'Hara <146030293+toharaIII@users.noreply.github.com>
2024-08-14 23:10:42 -0400

    prebreak commit
    added a menu and reorganized the appearance of things on the page, might be about to fuck everything up trying to get menu to not clip in instances of very large question div

--- desktop not signed in/desktopNSI.js
+++ desktop not signed in/desktopNSI.js
@@ -1,13 +1,22 @@
+//generates string newQuestion onto the page
 document.addEventListener('DOMContentLoaded', function() {
     function updateQuestion(newQuestion){
         const questionDiv=document.getElementsByClassName('question')[0];
         questionDiv.textContent=newQuestion
     }
 
-    let questionString="Is martyrdom for cowards?";
+    let questionString="how much deditated ram in the server for the server ya know i mean when i play minecraft on my server how much deditated ram do i need?";
     updateQuestion(questionString);
 });
 
+//changes the 3 bar menu to the X and adds show to the menu css id 
+document.getElementById('menuIcon').addEventListener('click', function(){
+    this.classList.toggle('change');
+    document.getElementById('menu').classList.toggle('show');
+    console.log("changing menu") //delete after debugged
+});
+
+//saves the content added to the text box to savedTextSpan... i thinnk
 document.addEventListener('DOMContentLoaded', function(){
     const answerBox=document.getElementById('userAnswer');
     const enterAnswer=document.getElementById('enterAnswer');
