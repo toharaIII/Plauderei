3e0d8efdeabe5f29bf3c900c4c2466b53e2f783a
Thomas O'Hara <146030293+toharaIII@users.noreply.github.com>
2024-08-18 18:01:07 -0400

    finished page
    everything on this page is finished except for the links in the menu, and the answer and response cnt being linked to the user and finally the response pop up needs to be finished once the database is done

--- desktop not signed in/desktopNSI.js
+++ desktop not signed in/desktopNSI.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
         questionDiv.textContent=newQuestion
     }
 
-    //let questionString="really big string like om gholy shit big string like zoo wee mama thats got to be at least 2 lines"
-    let questionString="Where does politics end and war begin?";
+    let questionString="really big string like om gholy shit big string like zoo wee mama thats got to be at least 2 lines"
+    //let questionString="Where does politics end and war begin?";
     updateQuestion(questionString);
 
     //changes the 3 bar menu to the X and adds show to the menu css id 
@@ -22,11 +22,15 @@ document.addEventListener('DOMContentLoaded', function() {
         //determine if the question div is wide enough to adjust when menu is present
         if(menuElement.classList.contains('show')){
             const isWide=questionDiv.offsetWidth>(menuQuestionLine.offsetWidth-100)
-            //if(isWide) menuQuestionLine.classList.toggle('menu-open');
             if(isWide) menuQuestionLine.classList.add('menu-open');
         } else menuQuestionLine.classList.remove('menu-open'); //removes adjustment for questionDiv if .show is not present
     });
 });
+
+let userAnswers=1;
+let userResponses=0;
+document.getElementById("answerCnt").textContent=userAnswers;
+document.getElementById("responseCnt").textContent=userResponses;
 
 //saves the content added to the text box to savedTextSpan... i thinnk
 document.addEventListener('DOMContentLoaded', function(){
@@ -49,5 +53,20 @@ document.addEventListener('DOMContentLoaded', function(){
             event.preventDefault();
             saveText();
         }
+    });
+});
+
+//creates the response popup when the question is clicked and removes it when anywhere else on the screen is clicked
+document.addEventListener('DOMContentLoaded', function(){
+    const questionDiv=document.querySelector('.question');
+    const popup=document.getElementById('responsePopUp');
+
+    questionDiv.addEventListener('click', function(event){
+        event.stopPropagation();
+        popup.classList.add('show-popup');
+    });
+
+    document.addEventListener('click', function(event){
+        if(!popup.contains(event.target) && event.target!==questionDiv) popup.classList.remove('show-popup');
     });
 });
