2073c650f6fb43ed438bafdac322f1a970051087
Thomas O'Hara <146030293+toharaIII@users.noreply.github.com>
2024-08-19 23:13:01 -0400

    work on myProfile page
    should have most things done for the users profile but nothing is done yet for their featured answers/threads, not sure how i want to do that one, also probably need more work on the friends list drop down since it doesnt seem to scroll at all

--- myProfile/myProfile.js
+++ myProfile/myProfile.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-        document.getElementById('menuIcon').addEventListener('click', function(){
+    let userAnswers=1;
+    let userResponses=0;
+    let userBadges=0;
+    let userName="Trey.Ohara";
+    let userFirst="Trey";
+    let userLast="O'Hara";
+    let dateJoined="August 2024";
+
+    //providing current answers and responses for given user, no need to update since cant reduce or add on this page
+    document.getElementById("answerCnt").textContent=userAnswers;
+    document.getElementById("responseCnt").textContent=userResponses;
+
+    document.getElementById("userName").textContent=userName;
+    document.getElementById("firstName").textContent=userFirst;
+    document.getElementById("lastName").textContent=userLast;
+    document.getElementById("dateJoined").textContent=dateJoined;
+
+    document.getElementById('menuIcon').addEventListener('click', function(){
         this.classList.toggle('change');
         const menuElement=document.getElementById('menu');
         const menuQuestionLine=document.querySelector('.menuQuestionLine');
@@ -8,9 +25,25 @@ document.addEventListener('DOMContentLoaded', function() {
         //to toggle menu visibility
         menuElement.classList.toggle('show');
     });
-});
 
-let userAnswers=1;
-let userResponses=0;
-document.getElementById("answerCnt").textContent=userAnswers;
-document.getElementById("responseCnt").textContent=userResponses;
+    function populationFriendsList(friendsCnt){
+        const friendsListContent=document.getElementById('friendsListContent');
+
+        friendsListContent.innerHTML=''; //this wipes the existing drop down content
+
+        //this actually populates the drop down
+        for(let i=1; i<=friendsCnt; i++){
+            const option=document.createElement('a');
+            option.href='#'; //link to a friends page, iterate thru database
+            option.textContent='friend ${i}'; //switch to the friends user name
+            friendsListContent.appendChild(option);
+        }
+    }
+    let friendCnt=15;
+    populationFriendsList(friendCnt);
+
+    document.querySelector('.showFriendsList').addEventListener('click', function(){
+        const friendsListContent=document.getElementById('friendsListContent');
+        friendsListContent.style.display=friendsListContent.style.display==='block'?'none':'block';
+    });
+});
