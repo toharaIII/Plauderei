0d5f6c91c9954e18fa10590ab787ebaf7b4a633e
Thomas O'Hara <146030293+toharaIII@users.noreply.github.com>
2024-08-21 23:26:03 -0400

    small adds and refactors
    everything thats on myProfile except the drop downs for submitted questions and friends are working 100% and have been refactor to my current best ability, however submitted questions drop down needs to be visually reformatted + add badge image next to questions that get picked and then both drop downs need to be placed under their button

--- myProfile/myProfile.js
+++ myProfile/myProfile.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     let userAnswers=1;
     let userResponses=0;
-    let badgesCnt=2;
+    let badgesCnt=35;
     let userName="Trey.Ohara";
     let userFirst="Trey";
     let userLast="O'Hara";
     let dateJoined="August 2024";
     let userEntry="hey, im the dev!";
     let friendCnt=35;
+    let submittedQuestionsArray=["Is determining when ai has reached agi a civil rights problem; as opposed to an engineering problem?",
+        "Is there a blanket statement you can about when it becomes worth it to break laws?",
+        "Is the inspiration for all non-hedonistic things legacy?"];
+
 
     //providing current answers and responses for given user, no need to update since cant reduce or add on this page
     document.getElementById("answerCnt").textContent=userAnswers;
@@ -18,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("lastName").textContent=userLast;
     document.getElementById("dateJoined").textContent=dateJoined;
     document.getElementById("userEntry").textContent=userEntry;
+    
 
     document.getElementById('menuIcon').addEventListener('click', function(){
         this.classList.toggle('change');
@@ -29,7 +34,37 @@ document.addEventListener('DOMContentLoaded', function() {
         menuElement.classList.toggle('show');
     });
 
-    function populationFriendsList(friendsCnt){
+    function determineMaxBadges(){
+        const profileInfoWidth=document.getElementById('profileInfo').offsetWidth;
+        const maxDisplayedBadges=Math.floor((profileInfoWidth-30)/30)-1
+        return maxDisplayedBadges;
+    };
+
+    function populateBadges(badgesCnt){
+        maxDisplayedBadges=determineMaxBadges(); //need to make this dynamic
+
+        const badgelocation=document.getElementById('badges');
+        badgelocation.innerHTML='';
+
+        badgesPrinted=badgesCnt<maxDisplayedBadges ? badgesCnt : maxDisplayedBadges; //determining number of badges to display
+        for(let i=0; i<badgesPrinted; i++){ //adding badges to top of profile
+            const badge=document.createElement('img');
+            badge.src='../../badge.png';
+            badgelocation.appendChild(badge);
+        }
+        if(badgesCnt>maxDisplayedBadges){ //print an integer if the user has more then 23 badges since thats how many can
+            badgeRemainder=badgesCnt-maxDisplayedBadges;
+            const additionalBadges=document.createElement('span');
+            additionalBadges.textContent=`+${badgeRemainder}`;
+            badgelocation.appendChild(additionalBadges);
+        }
+    }
+    populateBadges(badgesCnt);
+    window.addEventListener('resize', ()=> {
+        populateBadges(badgesCnt);
+    });
+
+    function populateFriendsList(friendsCnt){
         const friendsListContent=document.getElementById('friendsListContent');
         friendsListContent.innerHTML=''; //this wipes the existing drop down content
 
@@ -37,26 +72,40 @@ document.addEventListener('DOMContentLoaded', function() {
         for(let i=0; i<friendsCnt; i++){
             const friend=document.createElement('a');
             friend.href='#'; //link to a friends page, iterate thru database
-            friend.textContent='friend'; //switch to the friends user name
+            friend.textContent=`friend ${i+1}`; //switch to the friends user name
             friendsListContent.appendChild(friend);
         }
-    }
-    populationFriendsList(friendCnt);
-
-    function populateBadges(badgesCnt){
-        const badgelocation=document.getElementById('badges');
-        badgelocation.innerHTML='';
-
-        for(let i=0; i<badgesCnt; i++){
-            const badge=document.createElement('img');
-            badge.src='../../badge.png';
-            badgelocation.appendChild(badge);
-        }
-    }
-    populateBadges(badgesCnt);
-
+    };
+    populateFriendsList(friendCnt);
     document.querySelector('.showFriendsList').addEventListener('click', function(){
         const friendsListContent=document.getElementById('friendsListContent');
         friendsListContent.style.display=friendsListContent.style.display==='flex'?'none':'flex'; //cool different way of doing this, seems better then including a .show/.hide, switch all to this
+    });
+
+    enterQuestion=document.getElementById('submitQuestion');
+    function submitQuestionFromTextarea(submittedQuestionsArray){
+        questionTextarea=document.getElementById('userQuestion');
+        submittedQuestionsArray.push(questionTextarea.value);
+        populateSubmittedQuestions(submittedQuestionsArray);
+        questionTextarea.value='';
+    };
+    enterQuestion.addEventListener('click', function(){
+        submitQuestionFromTextarea(submittedQuestionsArray);
+    });
+
+    function populateSubmittedQuestions(submittedQuestionsArray){
+        const submittedQuestionsContent=document.getElementById('submittedQuestionsContent');
+        submittedQuestionsContent.innerHTML='';
+
+        for(let i=submittedQuestionsArray.length; i>=0; i--){
+            const question=document.createElement('div');
+            question.textContent=submittedQuestionsArray[i];
+            submittedQuestionsContent.appendChild(question);
+        }
+    };
+    populateSubmittedQuestions(submittedQuestionsArray);
+    document.querySelector('.showSubmittedQuestions').addEventListener('click', function(){
+        const submittedQuestionsContent=document.getElementById('submittedQuestionsContent');
+        submittedQuestionsContent.style.display=submittedQuestionsContent.style.display==='flex'?'none':'flex';
     });
 });
